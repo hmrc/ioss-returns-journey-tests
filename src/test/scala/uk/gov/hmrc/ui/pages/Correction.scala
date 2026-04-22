954e30d0430087c7a4046fc1a6e99317b72a6d5d
Susan Swinburne <susan.swinburne@opencastsoftware.com>
2026-04-22 12:06:46 +0100

    VEIOSS-858 wip for corrections spec

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/Correction.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+import org.junit.Assert
+import org.openqa.selenium.By
+import uk.gov.hmrc.selenium.webdriver.Driver
+
+object Correction extends BasePage {
+
+  def previouslyDeclaredText(traderType: String, display: Boolean): Unit = {
+    val htmlBody = Driver.instance.findElement(By.tagName("body")).getText
+
+    if (display) {
+      if (traderType == "NETP") {
+        Assert.assertTrue(
+          htmlBody.contains("Enter a minus value if too much was declared in the previous return.")
+        )
+        Assert.assertTrue(
+          htmlBody.contains("Their most recent declaration for this month is")
+        )
+      } else {
+        Assert.assertTrue(
+          htmlBody.contains("Enter a minus value if you declared too much in your previous return.")
+        )
+        Assert.assertTrue(
+          htmlBody.contains("Your most recent declaration for this month is")
+        )
+      }
+    } else {
+      Assert.assertFalse(
+        htmlBody.contains("Enter a minus value if you declared too much in your previous return.")
+      )
+      Assert.assertFalse(
+        htmlBody.contains("Your most recent declaration for this month is")
+      )
+    }
+  }
+
+  def noPaymentsDueForCorrectionsText(displayed: Boolean): Unit = {
+    val htmlBody = Driver.instance.findElement(By.tagName("body")).getText
+    if (displayed) {
+      Assert.assertTrue(
+        htmlBody.contains("VAT declared where no payment is due")
+      )
+      Assert.assertTrue(
+        htmlBody.contains(
+          "The tax authorities in countries where you declared too much VAT are responsible for refunding any overpayments."
+        )
+      )
+    } else {
+      Assert.assertFalse(
+        htmlBody.contains("VAT declared where no payment is due")
+      )
+      Assert.assertFalse(
+        htmlBody.contains(
+          "The tax authorities in countries where you declared too much VAT are responsible for refunding any overpayments."
+        )
+      )
+    }
+  }
+}
---
+++ src/test/scala/uk/gov/hmrc/ui/specs/ReturnsTests/CorrectionSpec.scala
@@ -0,0 +1,277 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs.ReturnsTests
+
+import uk.gov.hmrc.ui.pages._
+import uk.gov.hmrc.ui.specs.BaseSpec
+
+class CorrectionSpec extends BaseSpec {
+
+  private val dashboard = Dashboard
+  private val auth = Auth
+  private val fileUpload = FileUpload
+  private val correction = Correction
+
+  Feature("Corrections journeys") {
+
+    Scenario("A simple corrections journey with previously undeclared countries added to a nil return") {
+
+      Given("the user accesses the IOSS Returns Service")
+      auth.goToAuthorityWizard()
+      auth.loginUsingAuthorityWizard("100000001", "IM9008888888", "Organisation", "hasIOSSEnrolment", "dashboard")
+      dashboard.checkJourneyUrl("your-account")
+
+      When("the user clicks on the 'Start your return' link")
+      dashboard.clickLink("start-your-return")
+
+      Then("the user answers yes on the start page")
+      dashboard.checkJourneyUrl("IM9008888888/2023-M12/start-return")
+      dashboard.answerRadioButton("yes")
+
+      And("the user answers no on the want-to-upload-file page")
+      dashboard.checkJourneyUrl("IM9008888888/want-to-upload-file")
+      fileUpload.selectFileUpload("No, enter them myself")
+
+      And("the user answers no on the sold-goods page")
+      dashboard.checkJourneyUrl("IM9008888888/sold-goods")
+      dashboard.answerRadioButton("no")
+
+      And("the user answers yes on the correct-previous-return page")
+      dashboard.checkJourneyUrl("IM9008888888/correct-previous-return")
+      dashboard.answerRadioButton("yes")
+
+      And("the user answers yes on the correction-return-single-month page")
+      dashboard.checkJourneyUrl("IM9008888888/correction-return-single-month/1")
+      dashboard.answerRadioButton("yes")
+
+      And("the user selects a country")
+      dashboard.checkJourneyUrl("IM9008888888/correction-country/1/1")
+      dashboard.selectCountry("Estonia")
+
+      And("the user answers yes on the add-new-country page")
+      dashboard.checkJourneyUrl("IM9008888888/add-new-country/1/1")
+      dashboard.answerRadioButton("yes")
+
+      And("the country is not showing the previously declared text")
+      dashboard.checkJourneyUrl("IM9008888888/country-vat-correction-amount/1/1")
+      correction.previouslyDeclaredText("trader", display = false)
+
+      And("the user enters the correction amount")
+      dashboard.enterAnswer("1500")
+
+      And("the user answers yes on the vat-payable-confirm page")
+      dashboard.checkJourneyUrl("IM9008888888/vat-payable-confirm/1/1")
+      dashboard.answerRadioButton("yes")
+
+      And("the user clicks continue on the vat-payable-check page")
+      dashboard.checkJourneyUrl("IM9008888888/vat-payable-check/1/1")
+      dashboard.continue()
+
+      And("the user answers yes on the correction-list-countries page")
+      dashboard.checkJourneyUrl("IM9008888888/correction-list-countries/1")
+      dashboard.answerRadioButton("yes")
+
+      And("the user selects a country")
+      dashboard.checkJourneyUrl("IM9008888888/correction-country/1/2")
+      dashboard.selectCountry("Portugal")
+
+      And("the user answers yes on the add-new-country page")
+      dashboard.checkJourneyUrl("IM9008888888/add-new-country/1/2")
+      dashboard.answerRadioButton("yes")
+
+      And("the country is not showing the previously declared text")
+      dashboard.checkJourneyUrl("IM9008888888/country-vat-correction-amount/1/2")
+      correction.previouslyDeclaredText("trader", display = false)
+
+      And("the user enters the correction amount")
+      dashboard.enterAnswer("160.36")
+
+      And("the user answers yes on the vat-payable-confirm page")
+      dashboard.checkJourneyUrl("IM9008888888/vat-payable-confirm/1/2")
+      dashboard.answerRadioButton("yes")
+
+      And("the user clicks continue on the vat-payable-check page")
+      dashboard.checkJourneyUrl("IM9008888888/vat-payable-check/1/2")
+      dashboard.continue()
+
+      And("the user answers no on the correction-list-countries page")
+      dashboard.checkJourneyUrl("IM9008888888/correction-list-countries/1")
+      dashboard.answerRadioButton("no")
+
+      And("the user clicks continue on the vat-correction-months page")
+      dashboard.checkJourneyUrl("IM9008888888/2023-M12/vat-correction-months")
+      dashboard.continue()
+
+      And("the no payments due for minus corrections text is not displayed")
+      dashboard.checkJourneyUrl("IM9008888888/check-your-answers")
+      correction.noPaymentsDueForCorrectionsText(displayed = false)
+
+      And("the user submits their return successfully via the check-your-answers page")
+      dashboard.submit()
+      dashboard.checkJourneyUrl("IM9008888888/return-successfully-submitted")
+    }
+
+    Scenario("A corrections journey with previously declared countries") {
+
+      Given("the user accesses the IOSS Returns Service")
+      auth.goToAuthorityWizard()
+      auth.loginUsingAuthorityWizard("100000001", "IM9001234567", "Organisation", "hasIOSSEnrolment", "dashboard")
+      dashboard.checkJourneyUrl("your-account")
+
+      When("the user clicks on the 'Start your return' link")
+      dashboard.clickLink("start-your-return")
+
+      Then("the user answers yes on the start page")
+      dashboard.checkJourneyUrl("IM9001234567/2023-M12/start-return")
+      dashboard.answerRadioButton("yes")
+
+      And("the user answers no on the want-to-upload-file page")
+      dashboard.checkJourneyUrl("IM9001234567/want-to-upload-file")
+      fileUpload.selectFileUpload("No, enter them myself")
+
+      And("the user answers no on the sold-goods page")
+      dashboard.checkJourneyUrl("IM9001234567/sold-goods")
+      dashboard.answerRadioButton("no")
+
+      And("the user answers yes on the correct-previous-return page")
+      dashboard.checkJourneyUrl("IM9001234567/correct-previous-return")
+      dashboard.answerRadioButton("yes")
+
+      And("the user picks 2023 on the correction-return-year page")
+      dashboard.checkJourneyUrl("IM9001234567/correction-return-year/1")
+      dashboard.clickLink("value_2023")
+      dashboard.continue()
+
+      And("the user picks October on the correction-return-month page")
+      dashboard.checkJourneyUrl("IM9001234567/correction-return-month/1")
+      dashboard.clickLink("value_October")
+      dashboard.continue()
+
+      And("the user selects a country")
+      dashboard.checkJourneyUrl("IM9001234567/correction-country/1/1")
+      dashboard.selectCountry("Germany")
+
+      And("the country is showing the previously declared text")
+      dashboard.checkJourneyUrl("IM9001234567/country-vat-correction-amount/1/1")
+      correction.previouslyDeclaredText("trader", display = true)
+
+      And("the user enters the correction amount")
+      dashboard.enterAnswer("-100.65")
+
+      And("the user answers yes on the vat-payable-confirm page")
+      dashboard.checkJourneyUrl("IM9001234567/vat-payable-confirm/1/1")
+      dashboard.answerRadioButton("yes")
+
+      And("the user clicks continue on the vat-payable-check page")
+      dashboard.checkJourneyUrl("IM9001234567/vat-payable-check/1/1")
+      dashboard.continue()
+
+      And("the user answers yes on the correction-list-countries page")
+      dashboard.checkJourneyUrl("IM9001234567/correction-list-countries/1")
+      dashboard.answerRadioButton("yes")
+
+      And("the user selects a country")
+      dashboard.checkJourneyUrl("IM9001234567/correction-country/1/2")
+      dashboard.selectCountry("France")
+
+      And("the country is showing the previously declared text")
+      dashboard.checkJourneyUrl("IM9001234567/country-vat-correction-amount/1/2")
+      correction.previouslyDeclaredText("trader", display = true)
+
+      And("the user enters the correction amount")
+      dashboard.enterAnswer("1453")
+
+      And("the user answers yes on the vat-payable-confirm page")
+      dashboard.checkJourneyUrl("IM9001234567/vat-payable-confirm/1/2")
+      dashboard.answerRadioButton("yes")
+
+      And("the user clicks continue on the vat-payable-check page")
+      dashboard.checkJourneyUrl("IM9001234567/vat-payable-check/1/2")
+      dashboard.continue()
+
+      And("the user answers no on the correction-list-countries page")
+      dashboard.checkJourneyUrl("IM9001234567/correction-list-countries/1")
+      dashboard.answerRadioButton("no")
+
+      And("the user clicks yes on the vat-correction-months-add page")
+      dashboard.checkJourneyUrl("IM9001234567/2023-M12/vat-correction-months-add")
+      dashboard.answerRadioButton("yes")
+
+      And("the user picks 2023 on the correction-return-year page")
+      dashboard.checkJourneyUrl("IM9001234567/correction-return-year/2")
+      dashboard.clickLink("value_2023")
+      dashboard.continue()
+
+      And("the user picks November on the correction-return-month page")
+      dashboard.checkJourneyUrl("IM9001234567/correction-return-month/2")
+      dashboard.clickLink("value_November")
+      dashboard.continue()
+
+      And("the user selects a country")
+      dashboard.checkJourneyUrl("IM9001234567/correction-country/2/1")
+      dashboard.selectCountry("France")
+
+      And("the user enters the correction amount")
+      dashboard.checkJourneyUrl("IM9001234567/country-vat-correction-amount/2/1")
+      dashboard.enterAnswer("1500")
+
+      And("the user answers yes on the vat-payable-confirm page")
+      dashboard.checkJourneyUrl("IM9001234567/vat-payable-confirm/2/1")
+      dashboard.answerRadioButton("yes")
+
+      And("the user clicks continue on the vat-payable-check page")
+      dashboard.checkJourneyUrl("IM9001234567/vat-payable-check/2/1")
+      dashboard.continue()
+
+      And("the user answers yes on the correction-list-countries page")
+      dashboard.checkJourneyUrl("IM9001234567/correction-list-countries/2")
+      dashboard.answerRadioButton("yes")
+
+      And("the user selects a country")
+      dashboard.checkJourneyUrl("IM9001234567/correction-country/2/2")
+      dashboard.selectCountry("Germany")
+
+      And("the user enters the correction amount")
+      dashboard.checkJourneyUrl("IM9001234567/country-vat-correction-amount/2/2")
+      dashboard.enterAnswer("160.36")
+
+      And("the user answers yes on the vat-payable-confirm page")
+      dashboard.checkJourneyUrl("IM9001234567/vat-payable-confirm/2/2")
+      dashboard.answerRadioButton("yes")
+
+      And("the user clicks continue on the vat-payable-check page")
+      dashboard.checkJourneyUrl("IM9001234567/vat-payable-check/2/2")
+      dashboard.continue()
+
+      And("the user answers no on the correction-list-countries page")
+      dashboard.checkJourneyUrl("IM9001234567/correction-list-countries/2")
+      dashboard.answerRadioButton("no")
+
+      And("the user clicks continue on the vat-correction-months page")
+      dashboard.checkJourneyUrl("IM9001234567/2023-M12/vat-correction-months")
+      dashboard.continue()
+
+      And("the no payments due for minus corrections text is not displayed")
+      dashboard.checkJourneyUrl("IM9001234567/check-your-answers")
+      correction.noPaymentsDueForCorrectionsText(displayed = false)
+
+      And("the user submits their return successfully via the check-your-answers page")
+      dashboard.submit()
+      dashboard.checkJourneyUrl("IM9001234567/return-successfully-submitted")
+    }
+  }
+}
